0b274af62f26602116bf57c1af61cb8cddfb083a
NxGin97 <206642376+NxGin97@users.noreply.github.com>
2026-01-20 11:48:03 -0700

    info to course and staff is added

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,5 +1,33 @@
 import type { Course } from "../types/course";
 
 export const courses: Course[] = [
-  // add courses here
+  {
+    courseId: "CPRG303A2026",
+    courseCode: "CPRG303",
+    courseName: "Mobile Web Dev",
+    courseDescription: "Learning about web development in mobile devices",
+    credits: 3,
+    note: "Good luck"
+  },
+  {
+    courseId: "CPRG30A72026",
+    courseCode: "CPRG307",
+    courseName: "Database Programming II",
+    courseDescription: "Database Programming with database",
+    credits: 3,
+  },
+  {
+    courseId: "CPRG304A2026",
+    courseCode: "CPRG304",
+    courseName: "Object Oriented Programming 3",
+    courseDescription: "Programming object oriented 3",
+    credits: 3,
+  }, 
+  {
+    courseId: "CPRG306A2026",
+    courseCode: "CPRG306",
+    courseName: "Web Development II",
+    courseDescription: "Second semester web dev",
+    credits: 3,
+  }
 ];
